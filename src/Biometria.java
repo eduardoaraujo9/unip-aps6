e98784d749ced1a15d0aaee1c3ef42299b824fde
Eduardo Araujo <contatoedu@outlook.com>
2017-06-06 23:48:01 -0300

    hello world!

---
+++ src/Biometria.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Biometria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Software de Biometria");
+	}
+
+}
