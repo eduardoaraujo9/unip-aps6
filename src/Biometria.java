92225ecaaff4d991a54312f6cba89b51b4eeb721
Eduardo Araujo <contatoedu@outlook.com>
2017-08-15 20:36:27 -0300

    Estrutura inicial

--- src/Biometria.java
+++ src/Biometria.java
@@ -5,6 +5,14 @@ public class Biometria {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "Software de Biometria");
+		
+		CGMat vetorOriginal = new CGMat();
+		CGMat vetorTeste = new CGMat();
+		
+		// ao final validar com subtração = 0
+		if (vetorTeste.Subtracao(vetorOriginal) < 15) {
+			JOptionPane.showMessageDialog(null, "Bem vindo ao banco de dados do meio ambiente!");
+		}
 	}
 
 }
