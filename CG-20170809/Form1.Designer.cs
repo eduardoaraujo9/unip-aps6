ac3f6ed0b0d7d6cf767dfc7559c9d175d550d0ab
Eduardo Araujo <contatoedu@outlook.com>
2017-11-21 20:50:00 -0200

    mudanca de java para c#, aps completa

---
+++ CG-20170809/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(671, 445);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Sair";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.bemvindo;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(780, 481);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox5.Image = global::WindowsFormsApplication1.Properties.Resources.icon;
+            this.pictureBox5.Location = new System.Drawing.Point(310, 182);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(108, 115);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(38)))), ((int)(((byte)(174)))));
+            this.label2.Location = new System.Drawing.Point(44, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(653, 39);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Login biométrico de impressão digital";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(547, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 24);
+            this.label3.TabIndex = 15;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 478);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sistema";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CG-20170809/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap foto;
+        int[] h;
+        int[] h2;
+        int[] h3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog1.Filter = "Fotos|*.png;*.jpg;*.jpeg;*.gif;*.bmp";
+            openFileDialog1.Title = "Selecione a digital";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.foto = new Bitmap(openFileDialog1.FileName);
+
+                // Perfil de imagem
+                int[] f = new int[this.foto.Width];
+                int[] f2 = new int[this.foto.Width];
+                int[] f3 = new int[this.foto.Width];
+                int linha = this.foto.Height / 4;
+                int linha2 = this.foto.Height / 2;
+                int linha3 = linha + linha2;
+                for (int x = 0; x < this.foto.Width; x++)
+                {
+                    f[x] = foto.GetPixel(x, linha).R;
+                    f2[x] = foto.GetPixel(x, linha2).R;
+                    f3[x] = foto.GetPixel(x, linha3).R;
+                }
+                this.h = f;
+                this.h2 = f2;
+                this.h3 = f3;
+
+                int x0 = 0;
+                int y0 = 255 - f[0];
+                int y2 = 255 - f[1];
+                int y3 = 255 - f[2];
+                for (int x = 0; x < this.foto.Width; x++)
+                {
+                    x0 = x;
+                    y0 = 255 - f[x];
+                    y2 = 255 - f2[x];
+                    y3 = 255 - f3[x];
+                }
+
+                // amostragem do perfil
+                String t = ""; String t2 = ""; String t3 = "";
+                int l = 9; int[] v = new int[this.h.Count()]; int[] v2 = new int[this.h.Count()]; int[] v3 = new int[this.h.Count()];
+                int partes = this.h.Count() / 32; int acum = 0; int acum2 = 0; int acum3 = 0;
+                for (int x = 0; x < this.h.Count(); x++)
+                {
+                    for (int j = 0; j < partes; j++)
+                    {
+                        if (x + j >= this.h.Count()) { break; }
+                        // acumula e tira média
+                        acum += this.h[x + j] * l / this.foto.Height;
+                        acum2 += this.h2[x + j] * l / this.foto.Height;
+                        acum3 += this.h3[x + j] * l / this.foto.Height;
+                    }
+                    v[x] = acum / partes; acum = 0;
+                    v2[x] = acum2 / partes; acum2 = 0;
+                    v3[x] = acum3 / partes; acum3 = 0;
+                    t += v[x].ToString();
+                    t2 += v2[x].ToString();
+                    t3 += v3[x].ToString();
+
+                    x += partes;
+
+                }
+                t += t2 + t3;
+
+                // validação de login
+                Boolean login = false;
+                try
+                {
+                    using (StreamReader sr = new StreamReader("cadastro.txt"))
+                    {
+                        String line;
+                        string[] stringSeparators = new string[] { "," };
+                        string[] id;
+                        while ( ! sr.EndOfStream) {
+                            line = sr.ReadLine();
+                            id = line.Split(stringSeparators, StringSplitOptions.None);
+                            if (id[0] == t)
+                            {
+                                label3.Text = id[1].ToString();
+                                login = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Houve um erro consultando a base de dados, não foi possível prosseguir com a autenticação. " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                if (login)
+                {
+                    label1.Visible = true;
+                    label3.Visible = true;
+                    pictureBox1.Visible = true;
+                    //MessageBox.Show("Aprovado", "My Application", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                else
+                {
+                    MessageBox.Show("Acesso não autorizado.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            label1.Visible = false;
+            label3.Visible = false;
+        }
+    }
+}
